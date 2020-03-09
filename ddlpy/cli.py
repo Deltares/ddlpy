db4c834ac1eac36b886c91e7b6fa0ae8c1c4f781
Fedor Baart <fedor.baart@deltares.nl>
2020-03-09 10:53:53 +0100

    extend cli

--- ddlpy/cli.py
+++ ddlpy/cli.py
@@ -2,17 +2,65 @@
 
 """Console script for ddlpy."""
 import sys
+import io
+import logging
+
 import click
 
+import ddlpy
 
-@click.command()
-def main(args=None):
+
+@click.group()
+@click.option('-v', '--verbose', count=True)
+def cli(verbose,  args=None):
     """Console script for ddlpy."""
-    click.echo("Replace this message by putting your code into "
-               "ddlpy.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
+    level = logging.INFO
+    if verbose >= 1:
+        level = logging.DEBUG
+    logging.basicConfig(level=level)
     return 0
 
 
+@cli.command()
+@click.argument('output', type=click.File('w'))
+@click.option(
+    '--quantity',
+    help='Quantity (Grootheid) codes',
+    multiple=True
+)
+@click.option(
+    '--station',
+    help='Station codes',
+    multiple=True
+)
+@click.option(
+    '--format',
+    default='csv',
+    help='output file format',
+    type=click.Choice(['csv', 'json'], case_sensitive=True)
+)
+def locations(output, station, quantity, format):
+    """Write locations to output file"""
+    locations = ddlpy.locations()
+
+    # we have multiple of these, let's rename them
+    stations = station
+    quantities = quantity
+
+    selected = locations
+
+    if (stations):
+        selected = selected[selected.index.isin(stations)]
+    if quantities:
+        selected = selected[selected['Grootheid.Code'].isin(quantities)]
+
+    if format == 'csv':
+        selected.to_csv(output)
+    elif format == 'json':
+        selected.to_json(output, orient='records')
+    else:
+        raise ValueError('Unexpected format {}'.format(format))
+
+
 if __name__ == "__main__":
-    sys.exit(main())  # pragma: no cover
+    sys.exit(cli())  # pragma: no cover
