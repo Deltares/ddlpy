6a78a83be0c06bd8fe2af5af038df4a8dc837705
Fedor Baart <fedor.baart@deltares.nl>
2020-03-09 09:34:47 +0100

    expose functions

--- ddlpy/__init__.py
+++ ddlpy/__init__.py
@@ -5,3 +5,8 @@
 __author__ = """Fedor Baart"""
 __email__ = 'fedor.baart@deltares.nl'
 __version__ = '0.1.0'
+
+from ddlpy.ddlpy import locations
+from ddlpy.ddlpy import measurements
+
+__all__ = ['locations', 'measurements']
