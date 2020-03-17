163691e0430164f4b9f220fc92369b7f78f3deb6
Carmen Martinez Barbosa <Carmen.MartinezBarbosa@deltares.nl>
2020-03-17 14:53:55 +0100

    updates on minimal exmaple

--- examples/minimal_example.py
+++ examples/minimal_example.py
@@ -23,20 +23,20 @@ station= ['IJMDBTHVN', 'DANTZGZD','HOEKVHLD' ]
 # Filter the locations dataframe with the desired parameters and stations.
 selected= locations[locations.index.isin(station)]
 
-parameter = parameter[(parameter['Grootheid.Code'] == code) &
-                      (parameter['Hoedanigheid.Code'] == unit) ].reset_index()
+selected = selected[(selected['Grootheid.Code'] == code) &
+                      (selected['Hoedanigheid.Code'] == unit) ].reset_index()
 
 # Obtain measurements per parameter row
 index= 1
-location= parameter.loc[index]
+location= selected.loc[index]
 
-start_date = datetime.datetime(2005, 1, 1) # also inputs for the code
-end_date = datetime.datetime(2019, 3, 1)
+start_date = datetime.datetime(2015, 1, 1) # also inputs for the code
+end_date = datetime.datetime(2015, 12, 31)
 measurements = ddlpy.measurements(location, start_date=start_date, end_date=end_date)
 
 if (len(measurements) > 0):
     print('Data was found in Waterbase')
-    #measurements.to_csv(directory+"%s_%s.csv"%(location, donar_parcode), index= False)
+    #measurements.to_csv("%s_%s_%s.csv"%(location.Code, code, unit), index= False)
 else:
     print('No Data!')
 
