f880254dc0261256dba4e02dd054899950841fa5
veenstrajelmer <veenstrajelmer@gmail.com>
2024-03-08 15:03:36 +0100

    repo repairs, fixes #50

--- ddlpy/__init__.py
+++ ddlpy/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Fedor Baart"""
 __email__ = 'fedor.baart@deltares.nl'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 from ddlpy.ddlpy import locations
 from ddlpy.ddlpy import measurements, measurements_latest, simplify_dataframe
