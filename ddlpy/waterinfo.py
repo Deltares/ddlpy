2bd89d3d40885e04dc3386ec641bcdfdc36b2089
veenstrajelmer <60435591+veenstrajelmer@users.noreply.github.com>
2024-03-13 13:45:40 +0100

    fixed waterinfo.py quality issue (#84)
    * fixed waterinfo quality issue
    
    * fixed testcase
    
    * reran notebook

--- ddlpy/waterinfo.py
+++ ddlpy/waterinfo.py
@@ -133,8 +133,8 @@ def waterinfo_read(f, encoding='latin'):
             d['time'].attrs['timezone'] = 'UTC'
             
         ds.append(d)
-        
-    if len(ds)==1:
-        ds = ds[0]
-
+    
+    if len(ds)==0:
+        raise ValueError("no data available in file")
+    
     return ds
