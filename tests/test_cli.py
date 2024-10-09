3d0a1ce4c673554a10212e3aa65f8a9b07ce2bbb
veenstrajelmer <jelmer.veenstra@deltares.nl>
2024-10-09 15:46:31 +0200

    fixed cli test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -19,7 +19,7 @@ def test_command_line_interface():
     assert help_result.exit_code == 0
     assert 'Show this message and exit.' in help_result.output
 
-    locations_command = 'locations --grootheid-code WATHTE --station HOEKVHLD'
+    locations_command = 'locations --grootheid-code WATHTE --station hoekvanholland'
     locations_result = runner.invoke(cli.cli, locations_command.split())
     assert locations_result.exit_code == 0
     file_locs = "locations.json"
@@ -28,7 +28,7 @@ def test_command_line_interface():
     measurements_command = 'measurements 2023-01-01 2023-01-03'
     measurements_result = runner.invoke(cli.cli, measurements_command.split())
     assert measurements_result.exit_code == 0
-    file_meas = "HOEKVHLD_OW_cm_WATHTE_NVT_NAP_NVT.csv"
+    file_meas = "hoekvanholland_OW_cm_WATHTE_NVT_NAP_NVT.csv"
     assert os.path.exists(file_meas)
     
     # cleanup
