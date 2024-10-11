637427325c248a6746c7a7fff9a1ba3a74915bc3
veenstrajelmer <jelmer.veenstra@deltares.nl>
2024-10-11 02:02:03 +0200

    updated docs

--- docs/examples/minimal_example.py
+++ docs/examples/minimal_example.py
@@ -10,14 +10,17 @@ locations = ddlpy.locations()
 
 #select a set of parameters 
 # Filter the locations dataframe with the desired parameters and stations.
-bool_stations = locations.index.isin(['IJMDBTHVN', 'DANTZGZD','HOEKVHLD'])
-# measured (WATHTE) versus computed/astro
+bool_stations = locations.index.isin(['ijmuiden.buitenhaven', 'dantziggat.zuid', 'hoekvanholland', 'ameland.nes'])
+# meting/astronomisch/verwachting
+bool_procestype = locations['ProcesType'].isin(['meting'])
+# waterlevel/waterhoogte (WATHTE)
 bool_grootheid = locations['Grootheid.Code'].isin(['WATHTE'])
 # timeseries (NVT) versus extremes
-bool_groepering = locations['Groepering.Code'].isin(['NVT'])
+bool_groepering = locations['Groepering.Code'].isin([''])
 # vertical reference (NAP/MSL)
 bool_hoedanigheid = locations['Hoedanigheid.Code'].isin(['NAP'])
-selected = locations.loc[bool_stations & bool_grootheid & 
+selected = locations.loc[bool_procestype &
+                         bool_stations & bool_grootheid & 
                          bool_groepering & bool_hoedanigheid]
 
 start_date = dt.datetime(2023, 1, 1)
