947dfe667d6e26da6ce1aaaa0788bb9e3163099e
veenstrajelmer <veenstrajelmer@gmail.com>
2025-12-10 09:16:27 +0100

    added issue in comment

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -70,6 +70,7 @@ def test_collect_number_of_observations(collect_number_of_observations_resp):
 
 
 # TODO: AanvragenBulkWaarnemingen not present in beta WaterWebservices
+# https://github.com/Deltares/ddlpy/issues/145
 # @pytest.fixture
 # def request_bulk_observations_resp(endpoints):
 #     endpoint = endpoints['request_bulk_observations']
