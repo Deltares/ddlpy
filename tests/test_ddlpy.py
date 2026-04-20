1bb7fd9f35f1a5fc31716445e5dff4d73a60538b
veenstrajelmer <veenstrajelmer@gmail.com>
2026-04-20 09:36:30 +0200

    remove caching again

--- tests/test_ddlpy.py
+++ tests/test_ddlpy.py
@@ -8,7 +8,7 @@ import pytest
 import ddlpy
 import dateutil
 import numpy as np
-from ddlpy.ddlpy import _send_post_request, NoDataError, get_catalogfile_cache
+from ddlpy.ddlpy import _send_post_request, NoDataError
 
 DTYPES_NONSTRING = {
     "Locatie_MessageID": np.int64,
@@ -192,14 +192,6 @@ def test_send_post_request_errors_ophalenwaarnemingen(endpoints):
     with pytest.raises(NoDataError) as e:
         _send_post_request(url, request=request_nodata)
     assert "204 No Content:" in str(e.value)
-
-
-def test_get_catalogfile_cache():
-    catalogfile, use_cache = get_catalogfile_cache(catalog_filter=None)
-    assert use_cache is True
-
-    catalogfile, use_cache = get_catalogfile_cache(catalog_filter=[])
-    assert use_cache is False
 
 
 def test_nodataerror(location):
